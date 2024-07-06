21ebad25d99897886b34a7098a7337c7bd159160
GabryelDev <61790860+gabryeldias62@users.noreply.github.com>
2024-07-06 12:20:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import requests
+from tkinter import *
+
+
+
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+    texto_cotacoes["text"] = texto
+
+
+
+janela = Tk()
+janela.title("Cotação atual das moedas")
+janela.geometry("280x200")
+
+texto_orientacao = Label(janela, text="Clique no botão para ver a cotação das moedas!")
+texto_orientacao.grid(column=0, row=0, padx=10, pady=10)
+
+botao = Button(janela, text= "Buscar Cotaçôes", command=pegar_cotacoes)
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+texto_cotacoes = Label(janela, text="")
+texto_cotacoes.grid(column=0, row=2, padx=10, pady=10)
+
+
+janela.mainloop()
